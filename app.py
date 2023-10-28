b350a16168659e40afff446408d3aa91c745e2f9
b00nw33 <33589369+b00nw33@users.noreply.github.com>
2023-10-28 21:56:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,60 +1,23 @@
 # https://medium.com/@abhisheky127/mastering-pycaret-and-streamlit-a-comprehensive-guide-to-building-and-deploying-machine-learning-71b28a22655e
-from pycaret.classification import *
+!pip install pycaret -q
+!pip install streamlit -q
+
+from pycaret.classification import predict_model
 import streamlit as st
-import pandas as pd
-from pycaret.datasets import get_data
 
-iris = get_data('iris')
+model = load_model('diamond-pipeline')
+st.title('Your Model Prediction App')
 
-print('\nSetup')
-clf = setup(data=iris,
-            target='species',
-            session_id=123,
-            normalize=True,
-            transformation=True)
+# Collect user input
+input_1 = st.number_input('Input 1', min_value=0.0, max_value=10.0)
+input_2 = st.number_input('Input 2', min_value=0.0, max_value=10.0)
+input_3 = st.number_input('Input 3', min_value=0.0, max_value=10.0)
+input_4 = st.number_input('Input 4', min_value=0.0, max_value=10.0)
 
-print('\nCreate Model')
-model = create_model('lr')
-                    #  feature_selection=True,
-                    #  feature_interaction=True,
-                    #  feature_ratio=True)
-
-print('\nTune Model')
-tuned_model = tune_model(model, n_iter=50)
-
-# bagged_model = ensemble_model(model, method='Bagging')
-# blended_model = blend_models(estimator_list=[model1, model2, model3])
-# stacked_model = stack_models(estimator_list=[model1, model2],
-#                              meta_model=model3)
-
-# interpret_model(tuned_model, plot='summary')
-
-evaluate_model(tuned_model)
-
-# finalize the model
-final_best = finalize_model(tuned_model)
-
-# save model to disk
-save_model(final_best, 'diamond-pipeline')
-
-def main():
-    model = load_model('diamond-pipeline')
-    st.title('Your Model Prediction App')
-
-    # Collect user input
-    input_1 = st.number_input('Input 1', min_value=0.0, max_value=10.0)
-    input_2 = st.number_input('Input 2', min_value=0.0, max_value=10.0)
-    input_3 = st.number_input('Input 3', min_value=0.0, max_value=10.0)
-    input_4 = st.number_input('Input 4', min_value=0.0, max_value=10.0)
-
-    # Predict the output
-    if st.button('Predict'):
-        input_data = pd.DataFrame(
-            [[input_1, input_2, input_3, input_4]],
-            columns=['input_1', 'input_2', 'input_3', 'input_4'])
-        prediction = predict_model(model, data=input_data)
-        st.write(f"The predicted output is: {prediction['Label'].iloc[0]}")
-
-
-if __name__ == "__main__":
-    main()
+# Predict the output
+if st.button('Predict'):
+    input_data = pd.DataFrame(
+        [[input_1, input_2, input_3, input_4]],
+        columns=['input_1', 'input_2', 'input_3', 'input_4'])
+    prediction = predict_model(model, data=input_data)
+    st.write(f"The predicted output is: {prediction['Label'].iloc[0]}")
